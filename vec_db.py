aa7233ce4c636fa91f8b177804dbd010461fb575
mahmoudk07 <mahmoudkk177@gmail.com>
2023-12-25 21:56:19 +0200

    ✅ fix error

--- vec_db.py
+++ vec_db.py
@@ -213,7 +213,7 @@ class VecDB:
         return distances
             
 
-    def retrive(self, X: np.ndarray, k: int) -> tuple[np.ndarray, np.ndarray]:
+    def retrive(self, X: np.ndarray, k: int) -> list:
         """Find k closest database codes to given queries.
 
         Parameters
@@ -240,7 +240,7 @@ class VecDB:
             distances_all = self.compute_asymmetric_distances_2(X)
         
         indices = np.argsort(distances_all, axis=1)[:, :k]
-        return indices
+        return list(indices)
             
         """
             distances = np.empty((n_queries, k), dtype=np.float32)
